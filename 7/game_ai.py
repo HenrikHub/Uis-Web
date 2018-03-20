434f6f919981fdde1fb7ae7c0cbcb4f9e6f773f4
HenrikHub <henrik.egeland94@gmail.com>
2018-03-20 15:17:35 +0100

    hb

--- 7/game_ai.py
+++ 7/game_ai.py
@@ -7,8 +7,9 @@ import requests
 import time
 import random
 
+
 SERVER = "http://127.0.0.1:5000"  # this will not change
-TEAM_ID = "test"  # set it to your GitHub username
+TEAM_ID = "HenrikHub"  # set it to your GitHub username
 
 
 def reg():
@@ -20,8 +21,16 @@ def reg():
         print("Registered successfully as Player {}".format(resp["player"]))
         return resp["player"]
     else:
-        # TODO handle the case where the response was ERROR
-        pass
+
+        if resp["error_code"] == 1 or resp["error_code"] == 2:
+            print("TEAM_ID var ikke gyldig, vi prøver en gang til, med team navnet : 'SafeTeamID2321'")
+            resp = requests.get(SERVER + "/reg/" + "SafeTeamID2321").json()
+
+        elif resp["error_code"] == 3:
+            print("Spillet er allerede i gang. Vent til ny omgang!")
+            quit()
+
+
 
 
 def play(player):
@@ -35,12 +44,81 @@ def play(player):
             print("It's our turn ({}ms left)".format(status["time_left"]))
             # we make a random move => note that this might be an invalid move (segment may be occupied)
             # TODO: figure out a smart move
-            x = random.randint(0,6)
-            y = random.randint(0,6)
-            border = ["top", "right", "bottom", "left"][random.randint(0,3)]
-            print("Making a move: ({},{}) {}".format(x, y, border))
-            move = str(x) + "," + str(y) + "," + border
-            status = requests.get(SERVER + "/move/" + TEAM_ID + "/" + move).json()
+
+            if status["last_move"] != "":
+
+                border = []
+
+                lastmove = status["last_move"]
+                ##lastmovexy = [int(s) for s in lastmove.split() if s.isdigit()]
+                x = int(lastmove.split(",")[0])
+                y = int(lastmove.split(",")[1])
+                area = status["board"][x][y]
+
+                if area < 16:
+                    if area == 1 or area == 2 or area == 4 or area == 8:
+                        if area == 1:
+                            border = ["right","bottom", "left"][random.randint(0,2)]
+                        elif area == 2:
+                            border = ["top","bottom", "left"][random.randint(0,2)]
+                        elif area == 4:
+                            border = ["top","right", "left"][random.randint(0,2)]
+                        elif area == 8:
+                            border = ["top","right", "bottom"][random.randint(0,2)]
+
+                    elif area == 7 or area == 11 or area == 13 or area == 14:
+                        if area == 7:
+                            border = ["left"]
+                        elif area == 11:
+                            border = ["bottom"]
+                        elif area == 13:
+                            border = ["right"]
+                        elif area == 14:
+                            border = ["top"]
+
+                    print("Making a move: ({},{}) {}".format(x, y, border))
+                    move = str(x) + "," + str(y) + "," + border
+                    status = requests.get(SERVER + "/move/" + TEAM_ID + "/" + move).json()
+
+                elif area >= 16:
+
+                    minstearea = status["board"].min()
+                    xy = status["board"].index(status["board"].min ) ## Posisjonen til det minste tall i matrixen board
+                    x = xy[0]
+                    y = xy[1]
+
+                    if minstearea == 1 or minstearea == 2 or minstearea == 4 or minstearea == 8:
+                        if minstearea == 1:
+                            border = ["right", "bottom", "left"][random.randint(0, 2)]
+                        elif minstearea == 2:
+                            border = ["top", "bottom", "left"][random.randint(0, 2)]
+                        elif minstearea == 4:
+                            border = ["top", "right", "left"][random.randint(0, 2)]
+                        elif minstearea == 8:
+                            border = ["top", "right", "bottom"][random.randint(0, 2)]
+
+                    elif minstearea == 7 or minstearea == 11 or minstearea == 13 or minstearea == 14:
+                        if minstearea == 7:
+                            border = ["left"]
+                        elif minstearea == 11:
+                            border = ["bottom"]
+                        elif minstearea == 13:
+                            border = ["right"]
+                        elif minstearea == 14:
+                            border = ["top"]
+
+                print("Making a move: ({},{}) {}".format(x, y, border))
+                move = str(x) + "," + str(y) + "," + border
+                status = requests.get(SERVER + "/move/" + TEAM_ID + "/" + move).json()
+
+
+            else:
+                x = random.randint(0,6)
+                y = random.randint(0,6)
+                border = ["top", "right", "bottom", "left"][random.randint(0,3)]
+                print("Making a move: ({},{}) {}".format(x, y, border))
+                move = str(x) + "," + str(y) + "," + border
+                status = requests.get(SERVER + "/move/" + TEAM_ID + "/" + move).json()
 
 
 if __name__ == "__main__":
--- 7/test2.py
+++ 7/test2.py
@@ -1,35 +1,131 @@
+
 """
-Simple HTTP server.
-https://docs.python.org/3/library/http.server.html
+Game AI -- this is for you to complete
 """
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import requests
+import time
+import random
 
-class myHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-    """
-    HTTPRequestHandler class
-    Parsing of the request is done by the base class BaseHTTPRequestHandler.
-    """
 
-    # GET
-    def do_GET(self):
-        # Send response status code
-        self.send_response(200)
+SERVER = "http://127.0.0.1:5000"  # this will not change
+TEAM_ID = "HenrikHub2"  # set it to your GitHub username
 
-        # Send headers
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
 
-        message = "Hello world!"
-        # Write message content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
-        return
+def reg():
+    # register using a fixed team_id
+    resp = requests.get(SERVER + "/reg/" + TEAM_ID).json()  # register 1st team
 
-def main():
-    server_address = ('127.0.0.1', 8080)
-    httpd = HTTPServer(server_address, myHTTPServer_RequestHandler)
-    print("running server...")
-    httpd.serve_forever()
+    if resp["response"] == "OK":
+        # save which player we are
+        print("Registered successfully as Player {}".format(resp["player"]))
+        return resp["player"]
+    else:
+
+        if resp["error_code"] == 1 or resp["error_code"] == 2:
+            print("TEAM_ID var ikke gyldig, vi prøver en gang til, med team navnet : 'SafeTeamID2321'")
+            resp = requests.get(SERVER + "/reg/" + "SafeTeamID2321").json()
+
+        elif resp["error_code"] == 3:
+            print("Spillet er allerede i gang. Vent til ny omgang!")
+            quit()
+
+
+
+
+def play(player):
+    game_over = False
+    while not game_over:
+        time.sleep(0.2)  # wait a bit before making a new status request
+        status = requests.get(SERVER + "/status").json()  # request the status of the game
+        if status["status_code"] > 300:
+            game_over = True
+        elif status["status_code"] == 200 + player:  # it's our turn
+            print("It's our turn ({}ms left)".format(status["time_left"]))
+            # we make a random move => note that this might be an invalid move (segment may be occupied)
+            # TODO: figure out a smart move
+
+            if status["last_move"] != "":
+
+                border = []
+
+                lastmove = status["last_move"]
+                ##lastmovexy = [int(s) for s in lastmove.split() if s.isdigit()]
+
+                ## Converting list to string
+                x = ''.join((lastmove.split(",")[0]))
+                y = ''.join((lastmove.split(",")[1]))
+                int_x = int(lastmove.split(",")[0])
+                int_y = int(lastmove.split(",")[1])
+
+                area = status["board"][int_x][int_y]
+
+                if area < 16:
+                    if area == 1 or area == 2 or area == 4 or area == 8:
+                        if area == 1:
+                            border = ["right","bottom", "left"][random.randint(0,2)]
+                        elif area == 2:
+                            border = ["top","bottom", "left"][random.randint(0,2)]
+                        elif area == 4:
+                            border = ["top","right", "left"][random.randint(0,2)]
+                        elif area == 8:
+                            border = ["top","right", "bottom"][random.randint(0,2)]
+
+                    elif area == 7 or area == 11 or area == 13 or area == 14:
+                        if area == 7:
+                            border = ["left"]
+                        elif area == 11:
+                            border = ["bottom"]
+                        elif area == 13:
+                            border = ["right"]
+                        elif area == 14:
+                            border = ["top"]
+
+                    print("Making a move: ({},{}) {}".format(x, y, border))
+                    move = str(x) + "," + str(y) + "," + border
+                    status = requests.get(SERVER + "/move/" + TEAM_ID + "/" + move).json()
+
+                elif area >= 16:
+
+                    minstearea = status["board"].min()
+                    xy = status["board"].index(status["board"].min ) ## Posisjonen til det minste tall i matrixen board
+                    x = xy[0]
+                    y = xy[1]
+
+                    if minstearea == 1 or minstearea == 2 or minstearea == 4 or minstearea == 8:
+                        if minstearea == 1:
+                            border = ["right", "bottom", "left"][random.randint(0, 2)]
+                        elif minstearea == 2:
+                            border = ["top", "bottom", "left"][random.randint(0, 2)]
+                        elif minstearea == 4:
+                            border = ["top", "right", "left"][random.randint(0, 2)]
+                        elif minstearea == 8:
+                            border = ["top", "right", "bottom"][random.randint(0, 2)]
+
+                    elif minstearea == 7 or minstearea == 11 or minstearea == 13 or minstearea == 14:
+                        if minstearea == 7:
+                            border = ["left"]
+                        elif minstearea == 11:
+                            border = ["bottom"]
+                        elif minstearea == 13:
+                            border = ["right"]
+                        elif minstearea == 14:
+                            border = ["top"]
+
+                print("Making a move: ({},{}) {}".format(x, y, border))
+                move = str(x) + "," + str(y) + "," + border
+                status = requests.get(SERVER + "/move/" + TEAM_ID + "/" + move).json()
+
+
+            else:
+                x = random.randint(0,6)
+                y = random.randint(0,6)
+                border = ["top", "right", "bottom", "left"][random.randint(0,3)]
+                print("Making a move: ({},{}) {}".format(x, y, border))
+                move = str(x) + "," + str(y) + "," + border
+                status = requests.get(SERVER + "/move/" + TEAM_ID + "/" + move).json()
+
 
 if __name__ == "__main__":
-    main()
+    player = reg()
+    play(player)
